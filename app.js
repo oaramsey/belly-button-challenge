bd27ec7660aee05b7cb05ec6e5aff4d6e92d4054
oaramsey <129118228+oaramsey@users.noreply.github.com>
2023-09-19 20:38:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -15,15 +15,25 @@ d3.json(URL).then((data) => {
 
     buildMetadata(firstSample);
     buildCharts(firstSample);
+    
 });
 
-function buildMetadata(id) {
+
+
+function optionChanged(newSample) {
+    //Get new data
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+
+//Show demographics
+function buildMetadata(subjectID) {
     d3.json(URL).then((data) => {
-        console.log(id)
+        console.log(subjectID)
         let box = d3.select("#sample-metadata");
         let metadata = data.metadata;
 
-        let resultArray = metadata.filter(sampleObj => sampleObj.id == id);
+        let resultArray = metadata.filter(sampleObj => sampleObj.id == subjectID);
         console.log("resultArray")
         console.log(resultArray)
         let result = resultArray[0];
@@ -37,16 +47,13 @@ function buildMetadata(id) {
     })
 };
 
-
-
-
 //Building Charts
-function buildCharts(subjectID) {
-    console.log(subjectID);
+function buildCharts(sample) {
+    console.log(sample);
     d3.json(URL).then((data) => {
         let samples = data.samples;
 
-        let resultArray = samples.filter(sampleObj => sampleObj.id == subjectID);
+        let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
         console.log("resultArray")
         console.log(resultArray)
         let result = resultArray[0];
@@ -55,7 +62,6 @@ function buildCharts(subjectID) {
         let otu_labels = result.otu_labels;
         let sample_values = result.sample_values;
         console.log(otu_ids)
-
         let barData = [
             {
                 y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
@@ -72,6 +78,27 @@ function buildCharts(subjectID) {
         };
 
         Plotly.newPlot("bar", barData, barLayout);
+        let bubbleData = [
+            {
+                y: result.otu_ids,
+                x: result.sample_values,
+                text: result.otu_ids,
+                mode: "markers",
+                marker: {
+                    color: result.sample_values,
+                    colorscale: "Portland",
+                    size: result.sample_values,
+                    sizemode: "area"
+                },
+            }];
+        let bubbleLayout = {
+                title: "Bacteria Cultures Per Sample",
+                xaxis: {title: "OTU ID"}
+            };
+
+            Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+        
     })
     
 };
